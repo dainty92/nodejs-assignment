ec409b3ddc757cd55faa6c4964bdaac25c1080b2
Esther AJAYI <estheradexdainty@gmail.com>
2023-03-24 13:35:24 +0100

    assignment

---
+++ index.js
@@ -0,0 +1,5 @@
+// QUESTION 6: Create a javascript file and inside the file add code that will log in to the console, your name, your preferred programming language, and where you see yourself in the next 1 year.
+
+console.log("Hi there! I am Esther AJAYI.");
+console.log("My preferred programming language is JavaScript");
+console.log("In the next year, I hope to continue learning about IT tools and business concepts, which will enable me to deliver tangible value to clients.");
